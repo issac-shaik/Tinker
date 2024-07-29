50d87488e278a7b7a0d3ad920418194af06be21b
Issac Shaik <issac.shaik@gmail.com>
2024-07-29 19:18:19 +0530

    add sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,32 @@
+import { MenuSquare } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Sidebar = () => {
   return (
-    <div className="p-2 md:p-5">
-      <NewDocumentButton />
+    <div className="p-2 md:p-5 bg-slate-200 relative rounded-xl mx-2">
+      <Sheet>
+        <SheetTrigger>
+          <MenuSquare className="p-2 hover:opacity-30" size={40} />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <div>{/* options */}</div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+
+      <div className="hidden md:inline">
+        <NewDocumentButton />
+      </div>
     </div>
   );
 };
